72abc406312c1674ed57e04af2b69c0a07f7999f
saihemanthjakkula <saihemanthjakkula@gmail.com>
2020-09-14 20:44:13 +0530

    debugging

--- Events.php
+++ Events.php
@@ -3,11 +3,11 @@
 require ('db.php');
 $rest_json = file_get_contents("php://input");
 $headers = getallheaders();
-var_dump($headers);
+//var_dump($headers);
 //echo $headers['authorization'];
 //echo "working";
-if (isset($headers['authorization'])) {
-    $token = $headers['authorization'];
+if (isset($headers['Authorization'])) {
+    $token = $headers['Authorization'];
     $query_for_userid = "SELECT userid FROM users WHERE token = '$token' ";
     $result = $connect->query($query_for_userid);
     $feedData = mysqli_fetch_all($result,MYSQLI_ASSOC);
@@ -37,7 +37,7 @@ if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
 
         $query = "SELECT c.userid, c.event_name, c.event_date, c.event_start_time, c.event_end_time, c.event_id FROM calender_events c LEFT JOIN users u ON c.userid = u.userid WHERE u.userid='$logged_in_userid' ORDER BY c.event_date ";
         $result = $connect->query($query);
-echo $query;
+//echo $query;
 $feedData = mysqli_fetch_all($result,MYSQLI_ASSOC);
 $feedData=json_encode($feedData);
 
